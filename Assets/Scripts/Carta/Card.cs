579afef2f1c7ffb166a2e7044c92bdc8c2efcf6e
Higor Luiz <hluiz@id.uff.br>
2021-07-30 20:59:24 -0300

    Scriptable Objects funcionando

---
+++ Assets/Scripts/Carta/Card.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+	public string nome;
+	public string descricao;
+	public int pontos;
+	//public Sprite artwork;
+
+	public void Print(){
+
+	}
+
+}
---
+++ Assets/Scripts/Carta/CardDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class CardDisplay : MonoBehaviour {
+
+	public Card card;
+
+	public TextMeshPro nome;
+	public TextMeshPro descricao;
+	//public Image artwork;
+	public TextMeshPro pontos;
+	void Start () {
+		nome.text = card.nome;
+		descricao.text = card.descricao;
+		pontos.text = card.pontos.ToString();
+		
+		
+	}
+	
+}
